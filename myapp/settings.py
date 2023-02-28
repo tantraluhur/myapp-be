a07d37bed8e0db2a90f15934608188ad3620c4b5
aprilian tantra <tantraluhur@gmail.com>
2023-02-28 13:59:37 +0700

    add CORS

--- myapp/settings.py
+++ myapp/settings.py
@@ -117,6 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
 ]
